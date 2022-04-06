40b8095b177aeacf4ada22e20c11d9a0dedb3cbf
Tim Goshinski <tim@code-chimp.com>
2022-04-06 15:43:00 -0500

    :tada: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tailwind CSS + MVC Test
+
+Let's see how well I can make Tailwind and CSS play together.
---
+++ TailwindMVCTest/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['./Views/**/*.cshtml'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+};
